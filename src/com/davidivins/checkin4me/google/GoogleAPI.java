2c456d6829dbd2e0267f9a5b27ed089b9c14bb27
David Ivins <davidivins@gmail.com>
2011-03-24 00:23:03 -0400

    started google places integration...waiting on api client id key. applied for it.Signed-off-by: David Ivins <davidivins@gmail.com>

---
+++ src/com/davidivins/checkin4me/google/GoogleAPI.java
@@ -0,0 +1,464 @@
+////*****************************************************************************
+////    This file is part of CheckIn4Me.  Copyright © 2010  David Ivins
+////
+////    CheckIn4Me is free software: you can redistribute it and/or modify
+////    it under the terms of the GNU General Public License as published by
+////    the Free Software Foundation, either version 3 of the License, or
+////    (at your option) any later version.
+////
+////    CheckIn4Me is distributed in the hope that it will be useful,
+////    but WITHOUT ANY WARRANTY; without even the implied warranty of
+////    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+////    GNU General Public License for more details.
+////
+////    You should have received a copy of the GNU General Public License
+////    along with CheckIn4Me.  If not, see <http://www.gnu.org/licenses/>.
+////*****************************************************************************
+//package com.davidivins.checkin4me.google;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.HashMap;
+//import java.util.Properties;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import com.davidivins.checkin4me.comparators.LocaleDistanceComparator;
+//import com.davidivins.checkin4me.core.Locale;
+//import com.davidivins.checkin4me.interfaces.APIInterface;
+//import com.davidivins.checkin4me.oauth.OAuth2Request;
+//import com.davidivins.checkin4me.oauth.OAuthResponse;
+//
+//import android.content.SharedPreferences;
+//import android.util.Log;
+//
+///**
+// * GoogleAPI
+// * 
+// * @author david ivins
+// */
+//public class GoogleAPI implements APIInterface
+//{
+//	private static final String TAG = "GoogleAPI";
+//	
+//	private int service_id;
+//	private Properties config;
+//	
+//	private ArrayList<Locale> latest_locations;
+//	private boolean latest_checkin_status;
+//	
+//	/**
+//	 * GoogleAPI
+//	 * 
+//	 * @param Properties config
+//	 * @param int service_id
+//	 */
+//	public GoogleAPI(Properties config, int service_id)
+//	{
+//		this.config = config;
+//		this.service_id = service_id;
+//		latest_locations = new ArrayList<Locale>();
+//		latest_checkin_status = false;
+//	}
+//	
+//	/**
+//	 * getLocationThread
+//	 * 
+//	 * @param query
+//	 * @param longitude
+//	 * @param latitude
+//	 * @param persistent_storage
+//	 * @return LocationThread
+//	 */
+//	public Runnable getLocationThread(String query, String longitude, String latitude, SharedPreferences persistent_storage)
+//	{
+//		return new LocationThread(query, longitude, latitude, persistent_storage);
+//	}
+//	
+//	/**
+//	 * getLatestLocations
+//	 * 
+//	 * @return ArrayList<Locale>
+//	 */
+//	public ArrayList<Locale> getLatestLocations()
+//	{
+//		Collections.sort(latest_locations, new LocaleDistanceComparator());
+//		return latest_locations;
+//	}
+//	
+//	/**
+//	 * getCheckInThread
+//	 * 
+//	 * @param location
+//	 * @param persistent_storage
+//	 * @return CheckInThread
+//	 */
+//	public Runnable getCheckInThread(Locale location, String message, SharedPreferences persistent_storage)
+//	{
+//		latest_checkin_status = false;
+//		return new CheckInThread(location, message, persistent_storage);
+//	}
+//	
+//	/**
+//	 * getLatestCheckInStatus
+//	 * 
+//	 * @return boolean
+//	 */
+//	public boolean getLatestCheckInStatus()
+//	{
+//		return latest_checkin_status;
+//	}
+//	
+//	/**
+//	 * LocationThread
+//	 * 
+//	 * @author david
+//	 */
+//	class LocationThread implements Runnable
+//	{
+//		private String query;
+//		private String longitude;
+//		private String latitude;
+//		private SharedPreferences persistent_storage;
+//		
+//		/**
+//		 * LocationThread
+//		 * 
+//		 * @param query
+//		 * @param longitude
+//		 * @param latitude
+//		 */
+//		LocationThread(String query, String longitude, String latitude, SharedPreferences persistent_storage)
+//		{
+//			this.query     = query;
+//			this.longitude = longitude;
+//			this.latitude  = latitude;
+//			this.persistent_storage  = persistent_storage;
+//		}
+//
+//		/**
+//		 * run
+//		 */
+//		public void run() 
+//		{
+//			Log.i(TAG, "Retrieving Google Locations");
+//
+//			// build new oauth request
+//			OAuth2Request request = new OAuth2Request(
+//					config.getProperty("api_http_method"), config.getProperty("api_host"), 
+//					config.getProperty("api_version") + config.getProperty("api_locations_endpoint"));
+//			
+//			// set request headers
+//			request.addHeader("User-Agent", "CheckIn4Me:2.0");  // TODO: set this from meta-data
+//			
+//			// set query parameters
+//			if (query != null)
+//			{
+//				request.addQueryParameterAndEncode("query", query);
+//				request.addQueryParameter("limit", "10");
+//			}
+//			else
+//				request.addQueryParameter("limit", "50");
+//
+//			request.addQueryParameter("ll", latitude + "," + longitude);
+//			request.addQueryParameter("intent", "checkin");
+//			request.addQueryParameter("oauth_token", 
+//					persistent_storage.getString("google_oauth_token_secret", "GOOGLE_ACCESS_TOKEN_HERE"));
+//			
+//			// execute http request
+//			OAuthResponse response = (OAuthResponse)request.execute();
+//			
+//			// save locations
+//			if (response.getSuccessStatus())
+//				setLocationsFromJson(response.getResponseString(), query);	
+//		}
+//		
+//		/**
+//		 * setLocationsFromJson
+//		 * 
+//		 * @param json
+//		 */
+//		synchronized private void setLocationsFromJson(String json_string, String query)
+//		{
+//			Log.i(TAG, "json_string = " + json_string);
+//			if (null != query) Log.i(TAG, "query = " + query);
+//			
+//			// clear locations list
+//			latest_locations.clear();
+//			
+//			// get user's current location as doubles
+//			double user_longitude = Double.valueOf(persistent_storage.getString("current_longitude", "0.0"));
+//			double user_latitude  = Double.valueOf(persistent_storage.getString("current_latitude", "0.0"));
+//			
+//			// default to nearby places
+//			String type = "nearby";
+//			
+//			// if a query exists, look for the "places" group instead of "nearby"
+//			if (query != null)
+//				type = "places";
+//				
+//			try 
+//			{
+//				// get the json response string as a json object
+//				JSONObject full_response = new JSONObject(json_string);
+//				JSONObject response = full_response.getJSONObject("response");
+//				JSONArray groups = response.getJSONArray("groups");
+//				
+//				// loop through groups and find the group that is either the query results or the nearby places
+//				for (int i = 0; i < groups.length(); i++)
+//				{
+//					JSONObject current_object = groups.getJSONObject(i);
+//					
+//					// check the type of the current group
+//					if (current_object.getString("type").equals(type))
+//					{
+//						// get this group's venues
+//						JSONArray venues = current_object.getJSONArray("items");
+//						
+//						// store each venue as a new locale
+//						for (int j = 0; j < venues.length(); j++)
+//						{
+//							// get venue information
+//							JSONObject venue = venues.getJSONObject(j);
+//							
+//							String venue_id    = venue.getString("id");
+//							String name        = venue.getString("name");
+//							String description = "";
+//							
+//							// get venue location information
+//							JSONObject venue_location = venue.getJSONObject("location");
+//							
+//							String latitude  = venue_location.getString("lat");
+//							String longitude = venue_location.getString("lng");
+//							
+//							String address   = (venue_location.has("address")) ? venue_location.getString("address") : "";
+//							//String cross_street   = (venue_location.has("crossStreet")) ? venue_location.getString("crossStreet") : "";
+//							String city      = (venue_location.has("city")) ? venue_location.getString("city") : "";
+//							String state     = (venue_location.has("state")) ? venue_location.getString("state") : "";
+//							String zip       = (venue_location.has("postalCode")) ? venue_location.getString("postalCode") : "";
+//							//String distance   = (venue_location.has("distance")) ? venue_location.getString("distance") : "";
+//							//String country   = (venue_location.has("country")) ? venue_location.getString("country") : "";
+//
+//							// create a new locale object with the venue's data
+//							Locale location = new Locale(name, description, longitude, latitude,
+//									address, city, state, zip);
+//							location.calculateAndSetDistanceFromUser(user_longitude, user_latitude);
+//							location.mapServiceIdToLocationId(service_id, venue_id);
+//							
+//							// add the new locale to the latest locations list
+//							latest_locations.add(location);	
+//						}
+//
+//						break;
+//					}
+//				}
+//			} 
+//			catch (JSONException e) 
+//			{
+//				Log.e(TAG, "JSON Exception: " + e.getMessage());
+//				Log.e(TAG, "Could not parse json response: " + json_string);
+//			}
+//		}
+//	}
+//	
+//	/**
+//	 * CheckInThread
+//	 * 
+//	 * @author david
+//	 */
+//	class CheckInThread implements Runnable
+//	{
+//		private Locale location;
+//		private String message;
+//		private SharedPreferences persistent_storage;
+//		
+//		/**
+//		 * CheckInThread
+//		 * 
+//		 * @param location
+//		 * @param persistent_storage
+//		 */
+//		CheckInThread(Locale location, String message, SharedPreferences persistent_storage)
+//		{
+//			this.location = location;
+//			this.message = message;
+//			this.persistent_storage = persistent_storage;
+//		}
+//
+//		/**
+//		 * run
+//		 */
+//		public void run() 
+//		{
+//			Log.i(TAG, "Checking in on Google");
+//
+//			// build new oauth request
+//			OAuth2Request request = new OAuth2Request(
+//					config.getProperty("api_checkin_http_method"), config.getProperty("api_host"), 
+//					config.getProperty("api_version") + config.getProperty("api_checkin_endpoint"));
+//			
+//			// set request headers
+//			request.addHeader("User-Agent", "CheckIn4Me:1.0"); // TODO: get this from meta-data 
+//			
+//			// set query parameters
+//			request.addQueryParameter("oauth_token", 
+//					persistent_storage.getString("google_oauth_token_secret", "GOOGLE_ACCESS_TOKEN_HERE"));
+//			
+//			HashMap<Integer, String> service_id_location_id_xref = location.getServiceIdToLocationIdMap();
+//			String vid = service_id_location_id_xref.get(service_id);
+//			
+//			request.addQueryParameter("venueId", vid);
+//			request.addQueryParameter("ll", persistent_storage.getString("current_latitude", "CURRENT_LATITUDE_HERE") + "," +
+//					 persistent_storage.getString("current_longitude", "CURRENT_LONGITUDE_HERE"));
+//			
+//			// initialize broadcast to public
+//			String broadcast = "public";
+//			
+//			// if post to facebook is enabled, post it
+//			if (persistent_storage.getBoolean("google_post_to_facebook_default", false))
+//				broadcast += ",facebook";
+//			
+//			// if post to twitter is enabled, add it to broadcast
+//			if (persistent_storage.getBoolean("google_post_to_twitter_default", false))
+//				broadcast += ",twitter";
+//
+//			// add broadcast preferences to request
+//			request.addQueryParameterAndEncode("broadcast", broadcast);
+//			
+//			if (!message.equals(""))
+//				request.addQueryParameterAndEncode("shout", message);
+//			
+//			// execute http request
+//			OAuthResponse response = (OAuthResponse)request.execute();
+//			
+//			// save locations
+//			if (response.getSuccessStatus())
+//				setLocationsFromJson(response.getResponseString());	
+//		}
+//		
+//		/**
+//		 * setLocationsFromJson
+//		 * 
+//		 * @param json
+//		 */
+//		synchronized private void setLocationsFromJson(String json_string)
+//		{
+//			Log.i(TAG, "json_string = " + json_string);
+//			
+//			latest_checkin_status = false;
+//			
+//			try 
+//			{
+//				// get the json response string as a json object
+//				JSONObject full_response = new JSONObject(json_string);
+//				JSONObject response = full_response.getJSONObject("response");
+//				JSONObject checkin_info = response.getJSONObject("checkin");
+//				
+//				// get checkin status from fields returned in json response
+//				latest_checkin_status = checkin_info.has("id") && checkin_info.has("createdAt");
+//			} 
+//			catch (JSONException e) 
+//			{
+//				Log.i(TAG, "JSON Exception: " + e.getMessage());
+//				Log.i(TAG, "Could not parse json response: " + json_string);
+//			}
+//		}
+//	}
+//}
+//
+//import java.io.IOException;
+//import java.io.UnsupportedEncodingException;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import java.security.InvalidKeyException;
+//import java.security.NoSuchAlgorithmException;
+//import javax.crypto.Mac;
+//import javax.crypto.spec.SecretKeySpec;
+//import java.net.URL;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class UrlSigner {
+//
+// // Note: Generally, you should store your private key someplace safe
+// // and read them into your code
+//
+// private static String keyString = "YOUR_PRIVATE_KEY";
+//
+// // The URL shown in these examples is a static URL which should already
+// // be URL-encoded. In practice, you will likely have code
+// // which assembles your URL from user or web service input
+// // and plugs those values into its parameters.
+// private static String urlString = "YOUR_URL_TO_SIGN";
+//
+// // This variable stores the binary key, which is computed from the string (Base64) key
+// private static byte[] key;
+//
+// public static void main(String[] args) throws IOException,
+// InvalidKeyException, NoSuchAlgorithmException, URISyntaxException {
+//
+//   BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//
+//   String inputUrl, inputKey = null;
+//
+//   // For testing purposes, allow user input for the URL.
+//   // If no input is entered, use the static URL defined above.
+//   System.out.println("Enter the URL (must be URL-encoded) to sign: ");
+//   inputUrl = input.readLine();
+//   if (inputUrl.equals("")) {
+//     inputUrl = urlString;
+//   }
+//
+//   // Convert the string to a URL so we can parse it
+//   URL url = new URL(inputUrl);
+//
+//   // For testing purposes, allow user input for the private key.
+//   // If no input is entered, use the static key defined above.
+//   System.out.println("Enter the Private key to sign the URL: ");
+//   inputKey = input.readLine();
+//   if (inputKey.equals("")) {
+//     inputKey = keyString;
+//   }
+//
+//   UrlSigner signer = new UrlSigner(inputKey);
+//   String request = signer.signRequest(url.getPath(),url.getQuery());
+//
+//   System.out.println("Signed URL :" + url.getProtocol() + "://" + url.getHost() + request);
+// }
+//
+// public UrlSigner(String keyString) throws IOException {
+//   // Convert the key from 'web safe' base 64 to binary
+//   keyString = keyString.replace('-', '+');
+//   keyString = keyString.replace('_', '/');
+//   System.out.println("Key: " + keyString);
+//   this.key = Base64.decode(keyString);
+// }
+//
+// public String signRequest(String path, String query) throws NoSuchAlgorithmException,
+// InvalidKeyException, UnsupportedEncodingException, URISyntaxException {
+//
+//   // Retrieve the proper URL components to sign
+//   String resource = path + '?' + query;
+//
+//   // Get an HMAC-SHA1 signing key from the raw key bytes
+//   SecretKeySpec sha1Key = new SecretKeySpec(key, "HmacSHA1");
+//
+//   // Get an HMAC-SHA1 Mac instance and initialize it with the HMAC-SHA1 key
+//   Mac mac = Mac.getInstance("HmacSHA1");
+//   mac.init(sha1Key);
+//
+//   // compute the binary signature for the request
+//   byte[] sigBytes = mac.doFinal(resource.getBytes());
+//
+//   // base 64 encode the binary signature
+//   String signature = Base64.encodeBytes(sigBytes);
+//
+//   // convert the signature to 'web safe' base 64
+//   signature = signature.replace('+', '-');
+//   signature = signature.replace('/', '_');
+//
+//   return resource + "&signature=" + signature;
+// }
+//}
---
+++ src/com/davidivins/checkin4me/google/GoogleService.java
@@ -0,0 +1,219 @@
+//*****************************************************************************
+//    This file is part of CheckIn4Me.  Copyright © 2010  David Ivins
+//
+//    CheckIn4Me is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    CheckIn4Me is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with CheckIn4Me.  If not, see <http://www.gnu.org/licenses/>.
+//*****************************************************************************
+package com.davidivins.checkin4me.google;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import com.davidivins.checkin4me.comparators.ServiceSettingComparator;
+import com.davidivins.checkin4me.core.GeneratedResources;
+import com.davidivins.checkin4me.core.ServiceSetting;
+import com.davidivins.checkin4me.interfaces.APIInterface;
+import com.davidivins.checkin4me.interfaces.ServiceInterface;
+import com.davidivins.checkin4me.oauth.OAuthConnector;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.util.Log;
+
+/**
+ * GoogleService
+ * 
+ * @author david ivins
+ */
+public class GoogleService implements ServiceInterface
+{
+	private static final String TAG = "GoogleService";
+
+	private int service_id;
+	private SharedPreferences persistent_storage;
+	private Properties config;
+	private OAuthConnector oauth_connector;
+	private APIInterface api_adapter;
+	private HashMap<String, ServiceSetting> settings;
+	
+	/**
+	 * GoogleService
+	 * 
+	 * @param resources
+	 */
+	public GoogleService(int service_id, SharedPreferences persistent_storage, Resources resources)
+	{
+		// store service_id
+		this.service_id = service_id;
+		
+		// save pointer to persistent storage object
+		this.persistent_storage = persistent_storage;
+		
+		// read configuration file
+		config = new Properties();
+		
+		try 
+		{
+			InputStream config_file = resources.openRawResource(GeneratedResources.getRaw("google"));
+			config.load(config_file);
+			
+			// create oauth connector with current configuration
+//			oauth_connector =  new GoogleOAuthConnector(config);
+//			api_adapter = new GoogleAPI(config, service_id);
+		} 
+		catch (Exception e) 
+		{
+			Log.e(TAG, "Failed to open config file");
+		}
+		
+		// read settings xml
+		settings = new HashMap<String, ServiceSetting>();
+		
+		try 
+		{
+			InputStream is = resources.openRawResource(GeneratedResources.getRaw("google_settings"));
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document dom = builder.parse(is);
+			Element root = dom.getDocumentElement();
+			NodeList settings_nodes = root.getElementsByTagName("setting");
+
+			if (settings != null && settings_nodes.getLength() > 0) 
+			{
+				for (int i = 0 ; i < settings_nodes.getLength(); i++) 
+				{
+					Element setting_xml = (Element)settings_nodes.item(i);
+					ServiceSetting current_setting = new ServiceSetting(setting_xml, persistent_storage);
+					settings.put(current_setting.getPrefName(), current_setting);
+				}
+			}
+		} 
+		catch (Exception e) 
+		{
+			Log.e(TAG, "Failed to parse Google settings");
+		}
+	}
+	
+	/**
+	 * getId
+	 * 
+	 * @return int id
+	 */
+	public int getId()
+	{
+		return service_id;
+	}
+
+	/**
+	 * getName
+	 * 
+	 * @return String
+	 */
+	public String getName() 
+	{
+		return "Google";
+	}
+	
+	/**
+	 * getLogoDrawable
+	 * 
+	 * @return int
+	 */
+	public int getLogoDrawable()
+	{
+		return GeneratedResources.getDrawable("google_logo_resized");
+	}
+	
+	/**
+	 * getIconDrawable
+	 * 
+	 * @return int
+	 */
+	public int getIconDrawable()
+	{
+		return GeneratedResources.getDrawable("google25x25");
+	}
+
+	/**
+	 * getOAuthConnector
+	 * 
+	 * @return OAuthConnector
+	 */
+	public OAuthConnector getOAuthConnector() 
+	{
+		return oauth_connector;
+	}
+	
+	/**
+	 * getAPIInterface
+	 * 
+	 * @return APIInterface
+	 */
+	public APIInterface getAPIInterface()
+	{
+		return api_adapter;
+	}
+	
+	/**
+	 * connected
+	 * 
+	 * @return boolean
+	 */
+	public boolean connected()
+	{
+		return persistent_storage.contains("google_oauth_token_secret") && 
+			(persistent_storage.getString("google_oauth_token_secret", null) != null);
+	}
+	
+	/**
+	 * hasSettings
+	 */
+	public boolean hasSettings()
+	{
+		return (settings.size() > 0) ? true : false;
+	}
+	 
+	/**
+	 * getSettingsAsHashMap
+	 */
+	public HashMap<String, ServiceSetting> getSettingsAsHashMap()
+	{
+		return settings;
+	}
+	 
+	/**
+	 * getSettingsAsArrayList
+	 */
+	public ArrayList<ServiceSetting> getSettingsAsArrayList()
+	{
+		ArrayList<ServiceSetting> settings_list = new ArrayList<ServiceSetting>();
+	 
+		for (String key : settings.keySet())
+		{
+			settings_list.add(settings.get(key));
+		}
+	 
+		Collections.sort(settings_list, new ServiceSettingComparator());
+		return settings_list;
+	}
+}
